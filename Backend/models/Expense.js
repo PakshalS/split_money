17f611ac1c5ad1eccc828f0fdaa975f0fb770d13
Pakshal Shah <pakshalshah1927@gmail.com>
2024-06-26 16:42:58 +0530

    schema

---
+++ Backend/models/Expense.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  paidBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  group: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Group',
+    required: true,
+  },
+  splitAmong: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Expense', expenseSchema);
