d94fccdd1f619d1fa3a9798e0b37f83e66a0d1b6
Pakshal Shah <pakshalshah1927@gmail.com>
2024-06-28 18:30:24 +0530

    login api

--- Backend/Database/models/User.js
+++ Backend/Database/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,4 +17,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema ,'User' );
