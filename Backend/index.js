1e0babb448be961b632e91e73b435f2d3567d47f
Pakshal Shah <pakshalshah1927@gmail.com>
2024-08-07 22:33:55 +0530

    deploy changes'

--- Backend/index.js
+++ Backend/index.js
@@ -5,25 +5,18 @@ const friendrequestRoutes = require('./routes/friendrequestRoutes');
 const cors = require('cors');
 const dbConnect = require('./config/dbConnection');
 require('dotenv').config();
-const path = require('path');
+
 
 const app = express();
 const port = process.env.port;
 
 // Middleware
-app.use(cors(
-  // {
-  //   origin: ["https://deploy-split-money-api.vercel.app"],
-  //   methods:[
-  //     "POST" , "GET" , "DELETE" ,"PUT"
-  //   ],
-  //   credentials: true
-  // }
-));
+app.use(cors({
+  origin: ["https://split-money-alpha.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Routes
 app.use('/auth', authRoutes);
@@ -34,10 +27,6 @@ app.use('/friends',friendrequestRoutes);
 // Default Route
 app.get('/', (req, res) => {
   res.send('Hello, world!');
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Connect to MongoDB and start the server
