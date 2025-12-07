0398e587d30159808e46adc729faa2a34a0675fb
Pakshal Shah <110169361+PakshalS@users.noreply.github.com>
2025-12-08 01:35:11 +0530

    spelling change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,81 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./pages/Homepage";
-// import NoPage from "./pages/Error";
-// import Login from "./components/auth/Login";
-// import Register from "./components/auth/Signup";
-// import ProtectedRoute from "./components/protectedroute";
-// import Homee from "./pages/main2";
-// import Friends from "./components/secure/friends/friends";
-// import Settings from "./components/secure/settings/settings";
-// import GroupCreate from "./components/secure/create-group/creategroup";
-// import GroupDetails from "./components/secure/group/groupdetails";
-// import Layout from "./components/layout";
-// import ForgotPassReq from "./components/auth/ForgotPass";
-// import MainLayout from "./components/secure/shared/Layout";
-
-// function App() {
-//   return (
-//     <Routes>
-//       {/* Landing page WITHOUT Layout */}
-//       <Route path="/" element={<Home />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/forgot-password" element = {<ForgotPassReq/>} />
-        
-//       {/* Routes WITH Layout (persistent navbar) */}
-//       <Route path="/" element={<MainLayout/>}>
-//         <Route
-//           path="/home"
-//           element={
-//             <ProtectedRoute>
-//               <MainLayout/>
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/friends"
-//           element={
-//             <ProtectedRoute>
-//               <Friends />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/settings"
-//           element={
-//             <ProtectedRoute>
-//               <Settings />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/create-group"
-//           element={
-//             <ProtectedRoute>
-//               <GroupCreate />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Route>
-
-//       {/* Group Details WITHOUT Layout (no navbar) */}
-//       <Route
-//         path="/groups/:groupId"
-//         element={
-//           <ProtectedRoute>
-//             <GroupDetails />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       {/* Error page */}
-//       <Route path="*" element={<NoPage />} />
-//     </Routes>
-//   );
-// }
-
-// export default App;
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Homepage";
 import NoPage from "./pages/Error";
@@ -86,7 +8,7 @@ import Friends from "./components/secure/friends/friends";
 import Settings from "./components/secure/settings/settings";
 import GroupDetails from "./components/secure/group/groupdetails";
 import ForgotPassReq from "./components/auth/ForgotPass";
-import MainLayout from "./components/secure/shared/Layout";
+import MainLayout from "./components/secure/shared/layout";
 
 function App() {
   return (
